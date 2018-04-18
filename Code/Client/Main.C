98b93a9f1b28ce98320fc54dc66a7635a664c5b3
Inseok Yoon <diracyoon@gmail.com>
2018-04-18 16:08:37 +0200

    i'm tried

--- Code/Client/Main.C
+++ Code/Client/Main.C
@@ -1,18 +1,31 @@
+#include <cstdlib>
+
 #include "Client.h"
 
 int main(int argc, char* argv[])
 {
-  if(argc!=2)
+  if(argc!=5)
     {
       cerr << "Wrong number of argc." << endl;
+      cout << "argv[1]: runnumber, argv[2]: foil name, argv[3]: channel number, argv[4]: tester" << endl;
       exit(1);
     }
-  
-  int channel = atoi(argv[1]);
 
+  int runnumber = atoi(argv[1]);
+  string foil_name = argv[2];
+  int channel = atoi(argv[3]);
+  string tester = argv[4];
+  
+  char* path = getenv("QC_SW_PATH");
+  if(path==NULL)
+    {
+      cerr << "Enviromental variables aren't set. Source Env.sh first." << endl;
+      return -1;
+    }
+  
   try
     {
-      Client client("/home/isyoon/Test_HV_Control/Server/FIFO", channel);
+      Client client(runnumber, foil_name, channel, tester, path);
       client.Run();	
     }
   catch(const string& error)
