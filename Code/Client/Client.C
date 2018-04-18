0585836c8390e354c844271667a4c4b0d64378b3
Inseok Yoon <diracyoon@gmail.com>
2018-04-18 16:05:51 +0200

    writing

--- Code/Client/Client.C
+++ Code/Client/Client.C
@@ -2,8 +2,8 @@
 
 //////////
 
-Client::Client(const string& a_path_fifo_server, const int& a_channel) : path_fifo_server(a_path_fifo_server), channel(a_channel)
-{
+Client::Client(const int& a_runnumber, const string& a_foil_name, const int& a_channel, const string& a_tester, const string& a_path, const bool& a_verbosity) : runnumber(a_runnumber), foil_name(a_foil_name), channel(a_channel), tester(a_tester), path(a_path), verbosity(a_verbosity)
+{  
   Initialization();
 }//Client::Client()
 
@@ -15,6 +15,8 @@ Client::~Client()
   close(fd_server);
 
   unlink(path_fifo_client.c_str());
+
+  result_out.close();
 }//Client::~Client()
 
 //////////
@@ -65,8 +67,8 @@ void Client::Initialization()
   cout << "Initialize client for Ch." << channel << endl;
 
   //make FIFO for client
-  path_fifo_client = "/home/isyoon/Test_HV_Control/Client/FIFO" + to_string(channel);
-  
+ 
+  path_fifo_client = path + "/Macro/FIFO"  + to_string(channel);
   if(mkfifo(path_fifo_client.c_str(), 0666)==-1)
     {
       string error = "class Client: Can not make FIFO for client Ch" + to_string(channel) + ". Remove previous FIFO first.";
@@ -82,23 +84,12 @@ void Client::Initialization()
     }
   
   //open fifo for server
+  path_fifo_server = path + "/Macro/FIFO";
   fd_server = open(path_fifo_server.c_str(), O_WRONLY);//, O_NONBLOCK);   
   if(fd_server<0) throw "class Client: Can not open FIFO for server.";
   
   return;
 }//void Client::Initialization()
-
-//////////
-
-void Client::Initialization_QC()
-{
-  cout << "Initialize QC_Long." << endl;
-  
-  //trun on power
-  Request_HV_Control_Set("Pw", 1);
-  
-  return;
-}//void Client::Initialization_QC()
 
 //////////
 
@@ -114,7 +105,7 @@ string Client::Receive_From_Server()
 
   string msg = receive;
   
-  cout << "Server say:     " << msg << endl;
+  if(verbosity) cout << "Server say:     " << msg << endl;
       
   return msg;
 }//string Client::Receive_From_Server()
@@ -123,8 +114,10 @@ string Client::Receive_From_Server()
 
 void Client::Read_Config_Data()
 {
+  cout << "Read Config. data." << endl;
+  
   ifstream fin_config;
-  fin_config.open("/home/isyoon/GEM_QC2_Long_SW/Config");
+  fin_config.open("/home/isyoon/GEM_QC2_Long_SW/Config/Config.dat");
   if(!fin_config.is_open()) throw "class Client: Can not find the config file.";
 
   string buf;
@@ -135,54 +128,138 @@ void Client::Read_Config_Data()
       
       istringstream iss(buf);
 
-      int time;
-      int vset;
-      
-      buf >> time;
-      buf >> vset;
-
       Config_Data config_data;
+      
+      iss >> config_data.time;
+      iss >> config_data.vset;
+
       vec_config_data.push_back(config_data);
+
+      result_out << config_data.time << " " << config_data.vset << endl;
     }
+  result_out << "##############################" << endl;
   
   return;
 }//void Client::Read_Config_Data()
 
 //////////
 
-bool Client::Request_HV_Control_Get(const string& parameter, float& value)
+void Client::Recover_Trip(const float& vset)
 {
-  string transmit = "##GET## CH" + to_string(channel) + " " + parameter;
-  Transmit_To_Server(transmit);
+  static int n_trip;
+  n_trip++;
 
-  string result = Receive_From_Server();
+  cout << "class Client: Trip!! # of trip = " << n_trip << " Start recovery process." << endl;
+
+  Request_HV_Control_Set("Pw", 0);
+  Request_HV_Control_Set("Pw", 1);
+  Request_HV_Control_Set("VSet", vset);
   
-  if(result.find("##OK##")!=string::npos)
+
+  float vmon_old = -1;
+  while(1)
     {
-      istringstream iss(result);
+      float vmon = Request_HV_Control_Get("VMon");
 
-      string buf;
-      iss >> buf;
+      cout << "Recovering trip. VSet: " << vset << ", VMon = " << vmon << endl;
 
-      iss >> value;
+      //voltage stabilization condition
+      if(fabs(vmon-vset)/vset<0.05 && (vmon-vmon_old)<0.2)
+	{
+	  cout << "Recovery process done." << endl;
+	  break;
+	}
+
+      vmon_old = vmon;
       
-      return true;
+      this_thread::sleep_for(chrono::seconds(1));
     }
-  else return false;
+  
+  
+  return;
+}//void Client::Recover_Trip()
+
+//////////
+
+void Client::Result_Log_Maker()
+{
+  path_result = path + "/Output/" + to_string(runnumber) + "_" + foil_name + ".result";
+  result_out.open(path_result);
+
+  result_out << "Runnumber: " << runnumber << endl;
+  result_out << "Foil name: " << foil_name << endl;
+  result_out << "Channel: " << runnumber << endl;
+  result_out << "Tester: " << tester << endl;
+
+  time_t time = system_clock::to_time_t(system_clock::now());
+  result_out << "Start time: " << ctime(&time);
+  result_out << "##############################" << endl;
+  
+  return;
+}//void Client::Result_Log_Maker()
+
+//////////
+
+float Client::Request_HV_Control_Get(const string& parameter)
+{
+  for(int i=0; i<2; i++)
+    {
+      string transmit = "##GET## CH" + to_string(channel) + " " + parameter;
+      Transmit_To_Server(transmit);
+
+      string result = Receive_From_Server();
+  
+      if(result.find("##OK##")!=string::npos)
+	{
+	  istringstream iss(result);
+	  
+	  string buf;
+	  iss >> buf;
+
+	  float value;
+	  iss >> value;
+
+	  return value;
+	}
+
+      //if the first trial failed, wait 1 second and try one more time.
+      this_thread::sleep_for(chrono::seconds(1));
+    }
+
+  throw "class Client: HV control request failed.";
 }//bool Client::Request_HV_Control_Get(const string& parameter, int& value)
 
 //////////
 
-bool Client::Request_HV_Control_Set(const string& parameter, const float& value)
+void Client::Request_HV_Control_Set(const string& parameter, const float& value)
 {
-  string transmit = "##SET## CH" + to_string(channel) + " " + parameter + " " + to_string(value);
-  Transmit_To_Server(transmit);
+  for(int i=0; i<2; i++)
+    {
+      string transmit = "##SET## CH" + to_string(channel) + " " + parameter + " " + to_string(value);
+      Transmit_To_Server(transmit);
 
-  string result = Receive_From_Server();
+      string result = Receive_From_Server();
 
-  if(result.find("##OK##")!=string::npos) return true;
-  else return false;
+      if(result.find("##OK##")!=string::npos) return;
+
+      //if the first trial failed, wait 1 second and try one more time.
+      this_thread::sleep_for(chrono::seconds(1));
+    }
+
+  throw "class Client: HV control request failed.";
 }//void Client::Request_HV_Control_Set(const string& target, const int& value)
+
+//////////
+
+float Client::Scan_Config(const float& time_run)
+{
+  for(int i=0; i<vec_config_data.size(); i++)
+    {
+      if(vec_config_data[i].time<time_run && time_run<vec_config_data[i+1].time) return vec_config_data[i].vset;
+    }
+
+  return -1;
+}//float Client::Scan_Config(const duration<float> time_run)
 
 //////////
 
@@ -190,7 +267,7 @@ void Client::Transmit_To_Server(const string& msg)
 {
   if(write(fd_server, msg.c_str(), MSG_SIZE)<0) throw "class Client: Can not write on FIFO for server.";
 
-  cout << "Client Ch" << channel << " say: " << msg << endl;
+  if(verbosity) cout << "Client Ch" << channel << " say: " << msg << endl;
 
   return;
 }//void Client::Transmit_To_Server(const string& msg)
@@ -199,13 +276,19 @@ void Client::Transmit_To_Server(const string& msg)
 
 void Client::QC_Long()
 {
-  Initialization_QC();
+  QC_Long_Initialization();
 
   QC_Long_Body();
   
-  //finalize QC - turn off
+  //QC Long finalization - turn off
   Request_HV_Control_Set("Pw", 0);
 
+  result_out << "##############################" << endl;
+  time_t time = system_clock::to_time_t(system_clock::now());
+  result_out << "End time: " << ctime(&time);
+
+  cout << "QC Long done." << endl;
+  
   return;
 }//void Client::QC_Long()
 
@@ -213,20 +296,42 @@ void Client::QC_Long()
 
 void Client::QC_Long_Body()
 {
+  Result_Log_Maker();
+
   Read_Config_Data();
+    
+  cout << "Start QC2 Long test!!" << endl;
+
+  float vset_old = -999;
   
   system_clock::time_point time_start = system_clock::now();
-  
+
   while(1)
     {
-      system_clock::time_point time_current = system_clock::now();;
+      //run time
+      system_clock::time_point time_current = system_clock::now();
 
       duration<float> time_run = time_current - time_start;
-      cout << time_run.count() << endl;
       
-      float value;
-      Request_HV_Control_Get("IMonL", value);
+      float vset = Scan_Config(time_run.count());
+      
+      //end of QC
+      if(vset<0) break;
+      else if(vset!=vset_old)
+	{
+	  Request_HV_Control_Set("VSet", vset);
+	  vset_old = vset;
+	}
+      
+      if(Request_HV_Control_Get("Pw")==false) Recover_Trip(vset);
+            
+      float vmon = Request_HV_Control_Get("VMon");
+      float imon = Request_HV_Control_Get("IMonH");
 
+      result_out << time_run.count() << " " << vset << " " << vmon << " " << imon << endl; 
+
+      if(int(time_run.count())%10==0) cout << "Run duration = " << time_run.count() << "s, V_set = " << vset << "V, V_Mon = " << vmon << "V, I_Mon = " << imon << "uA" << endl; 
+      
       this_thread::sleep_for(chrono::seconds(1));
     }  
   
@@ -234,3 +339,22 @@ void Client::QC_Long_Body()
 }//void Client::QC_Long_Body()
 
 //////////
+
+void Client::QC_Long_Initialization()
+{
+  cout << "Initialize QC_Long." << endl;
+
+  Request_HV_Control_Set("Pw", 1);
+
+  Request_HV_Control_Set("MaxV", 615);
+  Request_HV_Control_Set("RUp", 1);
+  Request_HV_Control_Set("RDwn", 50);
+
+  Request_HV_Control_Set("ISet", 1);
+  Request_HV_Control_Set("Trip", 0.1);
+  Request_HV_Control_Set("PDwn", 0);
+    
+  return;
+}//void Client::QC_Long_Initialization()
+
+/////////
--- Code/Client/Client.h
+++ Code/Client/Client.h
@@ -5,6 +5,7 @@
 #include <thread>
 #include <sstream>
 #include <vector>
+#include <cmath>
 
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -18,27 +19,36 @@ using namespace chrono;
 class Client
 {
  public:
-  Client(const string& a_path_fifo_server, const int& a_channel);
+  Client(const int& a_runnumber, const string& a_foil_name, const int& a_channel, const string& a_tester, const string& a_path, const bool& a_verbosity=false);
   ~Client();
 
   void Run();
 
   typedef struct _config_data
   {
-    int time;
+    float time;
     float vset;
   } Config_Data;
   
  protected:
+  int runnumber;
+  string foil_name;
   int channel;
+  string tester;
 
+  bool verbosity;
+
+  string path;
   string path_fifo_client;
   string path_fifo_server;
   
   int fd_client;
   int fd_server;
-
+  
   vector<Config_Data> vec_config_data;
+
+  string path_result;
+  ofstream result_out;
   
   void Connect_To_Server();
   void Disconnect_From_Server();
@@ -46,9 +56,13 @@ class Client
   void Initialization_QC();
   string Receive_From_Server();
   void Read_Config_Data();
-  bool Request_HV_Control_Get(const string& parameter, float& value);
-  bool Request_HV_Control_Set(const string& parameter, const float& value);
+  void Recover_Trip(const float& vset);
+  void Result_Log_Maker();
+  float Request_HV_Control_Get(const string& parameter);
+  void Request_HV_Control_Set(const string& parameter, const float& value);
+  float Scan_Config(const float& time_run);
   void Transmit_To_Server(const string& msg);
   void QC_Long();
   void QC_Long_Body();
+  void QC_Long_Initialization();
 };
