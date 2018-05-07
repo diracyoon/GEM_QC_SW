5e1bfdb06b1c05ffcad60de97f2f09d2d017f242
helee <haneol.lee@cern.ch>
2018-05-07 16:30:16 +0200

    update drawing options

--- Code/Include/Painter.h
+++ Code/Include/Painter.h
@@ -30,8 +30,24 @@ class Painter
   
   string file_path;
   ifstream fin;  
-  
+ 
+  float xmin;
+  float xmax;
+  float y1min;
+  float y1max;
+  float y2min;
+  float y2max;
+
+  TString title;
+  TString text1;
+  TString text2;
+
+  TMultiGraph* gr; 
   TGraphErrors* gr_vset;
   TGraphErrors* gr_vmon;
   TGraphErrors* gr_imon;
+  TGaxis* axis;
+  TLegend* legend;
+  TLatex* latex1;
+  TLatex* latex2;
 };
--- Code/Src/Painter.C
+++ Code/Src/Painter.C
@@ -55,15 +55,73 @@ Painter::Painter(const string& a_file_path) : file_path(a_file_path)
 
       if(chk_separater==2) break;
     }
-  
+
+  //multigraph
+  gr = new TMultiGraph(); 
   gr_vset = new TGraphErrors();
-  gr_vset->SetName("V_Set");
-  gr_vset->SetTitle("Time vs. V_{Set}");
-  gr_vset->GetXaxis()->SetTitle("Time [s]");
-  gr_vset->GetYaxis()->SetTitle("V_{Set} [V]");
-  
   gr_vmon = new TGraphErrors();
   gr_imon = new TGraphErrors();
+
+  gr->Add(gr_vset);
+  gr->Add(gr_vmon);
+  gr->Add(gr_imon);
+
+  title = Form("Run : %d, Foil : %s , Channel : %d", runnumber, foil_name.Data(), channel);
+  gr->SetTitle(title);
+  gr->GetXaxis()->SetTitle("Time (s)");
+  gr->GetYaxis()->SetTitle("Voltage (V)");
+  gr->GetXaxis()->SetTitleSize(0.04);
+  gr->GetXaxis()->SetLabelSize(0.04);
+  gr->GetYaxis()->SetTitleSize(0.04);
+  gr->GetYaxis()->SetLabelSize(0.04);
+
+  //Set the axis range
+  float timemax = TMath::MaxElement(gr_vset->GetN(),gr_vset->GetX());
+  float vsetmax = TMath::MaxElement(gr_vset->GetN(),gr_vset->GetY());
+  float imonmax = TMath::MinElement(gr_imon->GetN(),gr_imon->GetY());
+
+  xmin = 0.; xmax = timemax + 10.;
+  y1min = 0.; y1max = vsetmax + 10.;
+  y2min = 0.; y2max = imonmax + 10.;
+
+  gr->SetMinimum(y1min);
+  gr->SetMaximum(y1max);
+  gr->GetXaxis()->SetLimits(xmin, xmax);
+
+  //Set the 2nd yaxis
+  axis = new TGaxis(xmax, y1min, xmax, y1max, y2min, y2max, 510, "+L");
+  axis->SetTitle("Current (#muA)");
+  axis->SetTitleSize(0.04);
+  axis->SetTitleFont(42);
+  axis->SetLabelSize(0.04);
+  axis->SetLabelFont(42);
+  axis->Draw();
+
+  //additional text
+  text1 = Form("Humidity : %f%", rh);
+  text2 = Form("Temperature : %f#circC", temperature);
+
+  latex1 = new TLatex(.13, .87, text1);
+  latex1->SetNDC();
+  latex1->SetTextSize(0.04);
+  latex1->SetTextAlign(12);
+  latex1->SetTextFont(42);
+  latex1->Draw();
+
+  latex2 = new TLatex(.13, .82, text2);
+  latex2->SetNDC();
+  latex2->SetTextSize(0.04);
+  latex2->SetTextAlign(12);
+  latex2->SetTextFont(42);
+  latex2->Draw();
+
+  //legend
+  legend = new TLegend(.77, .75, .87, .87);
+  legend->AddEntry(gr_vset, "V_{set}", "l");
+  legend->AddEntry(gr_vmon, "V_{mon}", "l");
+  legend->AddEntry(gr_imon, "I_{mon}", "l");
+  legend->SetLineWidth(0);
+  legend->Draw();
 }//Painter::Painter()
 
 //////////
@@ -127,12 +185,32 @@ void Painter::Update()
 
       //add point to graphs
       int n_point = gr_vset->GetN();
-
       gr_vset->SetPoint(n_point, time, vset);
-
       gr_vmon->SetPoint(n_point, time, vmon);
-      
       gr_imon->SetPoint(n_point, time, imon);
+
+      //match the curret(I_set) scale to the voltage scale
+      float vsetmax = TMath::MaxElement(gr_vset->GetN(),gr_vset->GetY());
+      float imonmax = TMath::MaxElement(gr_imon->GetN(),gr_imon->GetY());
+      float scale = (vsetmax + 10.)/(imonmax + 10.);
+      imon = scale*imon;
+      gr_imon->SetPoint(n_point, time, imon);
+
+      //graph style
+      gr_vset->SetMarkerStyle(20);
+      gr_vset->SetMarkerColor(kRed);
+      gr_vset->SetLineColor(kRed);
+      gr_vset->SetLineWidth(2);
+
+      gr_vmon->SetMarkerStyle(20);
+      gr_vmon->SetMarkerColor(kOrange);
+      gr_vmon->SetLineColor(kOrange);
+      gr_vmon->SetLineWidth(2);
+
+      gr_imon->SetMarkerStyle(20);
+      gr_imon->SetMarkerColor(kBlue);
+      gr_imon->SetLineColor(kBlue);
+      gr_imon->SetLineWidth(2);
     }
   
   return;
